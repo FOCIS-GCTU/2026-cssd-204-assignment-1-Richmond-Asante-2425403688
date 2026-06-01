828739325dc0a1c2bd1733a2f63b0e58607457ea
Richmond-Asante-2425403688 <2425403688@live.gctu.edu.gh>
2026-06-01 10:41:20 -0800

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Asante Richmond
+# STUDENT ID: 2425403688
+# Email: 2425403688@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Asante Richmond, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #code to print out my initials ARR
 
+    print("print out my initials\n\n")
+    
+    print("...ARR\n\n")
+    print()
+
+    print("...AAAAAAAAAAAA****RRRRRRRRRRRR****RRRRRRRRRRRR")
+    print("...AA........AA****RR........RR****RR........RR")
+    print("...AA........AA****RR........RR****RR........RR")
+    print("...AA........AA****RR........RR****RR........RR")
+    print("...AAAAAAAAAAAA****RRRRRRRRRRRR****RRRRRRRRRRRR")
+    print("...AA........AA****RRRRRRRR....****RRRRRRRR....")
+    print("...AA........AA****RR....RR....****RR....RR....")
+    print("...AA........AA****RR.....RR...****RR.....RR...")
+    print("...AA........AA****RR......RR..****RR......RR..")
+    print("...AA........AA****RR.......RR.****RR.......RR.")
+    print()
 
 
 main()
